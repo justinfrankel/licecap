8c6ca86f2b8736ca24d5fa0c418c4d6cc77854e9
Cockos Inc <nospam@cockos.com>
2012-02-11 10:50:39 -0500

    libpng 1.5.8 -- from 9d6982cf

--- WDL/libpng/pngtest.c
+++ WDL/libpng/pngtest.c
@@ -1817,4 +1817,4 @@ main(int argc, char *argv[])
 }
 
 /* Generate a compiler error if there is an old png.h in the search path. */
-typedef png_libpng_version_1_5_7 Your_png_h_is_not_version_1_5_7;
+typedef png_libpng_version_1_5_8 Your_png_h_is_not_version_1_5_8;
