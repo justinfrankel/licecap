cca8335906f7bb28f54e852de8a9054e78050342
Cockos Inc <nospam@cockos.com>
2012-08-20 21:32:07 -0400

    LICE: support for tRNS chunks fixed (thanks Tale) -- from 1f683b5a

--- WDL/lice/lice_png.cpp
+++ WDL/lice/lice_png.cpp
@@ -71,7 +71,10 @@ LICE_IBitmap *LICE_LoadPNG(const char *filename, LICE_IBitmap *bmp)
     png_set_expand_gray_1_2_4_to_8(png_ptr);
 
   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) 
+  {
     png_set_tRNS_to_alpha(png_ptr);
+    color_type |= PNG_COLOR_MASK_ALPHA;
+  }
 
   if (bit_depth == 16)
     png_set_strip_16(png_ptr);
@@ -241,7 +244,10 @@ LICE_IBitmap *LICE_LoadPNGFromMemory(const void *data_in, int buflen, LICE_IBitm
     png_set_expand_gray_1_2_4_to_8(png_ptr);
 
   if (png_get_valid(png_ptr, info_ptr, PNG_INFO_tRNS)) 
+  {
     png_set_tRNS_to_alpha(png_ptr);
+    color_type |= PNG_COLOR_MASK_ALPHA;
+  }
 
   if (bit_depth == 16)
     png_set_strip_16(png_ptr);
