adf18364ee2e9dac91a5ad94fb6701f6c1070d5a
Cockos Inc <nospam@cockos.com>
2012-08-24 22:39:19 -0400

    db2val: added VAL2DB_EX() which allows specifying a lower bound -- from 119677bf

--- WDL/db2val.h
+++ WDL/db2val.h
@@ -6,6 +6,7 @@
 #define TWENTY_OVER_LN10 8.6858896380650365530225783783321
 #define LN10_OVER_TWENTY 0.11512925464970228420089957273422
 #define DB2VAL(x) exp((x)*LN10_OVER_TWENTY)
+
 static inline double VAL2DB(double x)
 {
   if (x < 0.0000000298023223876953125) return -150.0;
@@ -13,5 +14,9 @@ static inline double VAL2DB(double x)
   return v<-150.0?-150.0:v;
 }
 
+static inline double VAL2DB_EX(double x, double mindb)
+{
+  return x <= DB2VAL(mindb) ? mindb : (log(x)*TWENTY_OVER_LN10);
+}
 
 #endif
