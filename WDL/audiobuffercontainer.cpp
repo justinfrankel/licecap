a4d9ba8f8b24d81329299248b9f892ef5e518de6
Cockos Inc <nospam@cockos.com>
2013-07-03 14:32:54 -0400

    AudioBufferContainer: fixed Resize() preserving data when changing channel counts -- from 0bb255d7

--- WDL/audiobuffercontainer.cpp
+++ WDL/audiobuffercontainer.cpp
@@ -229,11 +229,43 @@ void AudioBufferContainer::Resize(int nCh, int nFrames, bool preserveData)
     preserveData = false;
   }
 
-  int newsz = nCh*nFrames*(int)m_fmt;
+  const int newsz = nCh*nFrames*(int)m_fmt;
 
   if (preserveData && (nCh != m_nCh || nFrames != m_nFrames))
   {
     GetAllChannels(m_fmt, true);  // causes m_data to be interleaved
+
+    if (newsz > m_data.GetSize()) m_data.Resize(newsz);
+    if (nCh != m_nCh && m_data.GetSize() >= newsz)
+    {
+      char *out = (char *)m_data.Get();
+      const char *in = out;
+      const int in_adv = m_nCh * m_fmt, out_adv = nCh * m_fmt;
+      const int copysz = min(in_adv,out_adv);
+
+      int n = min(nFrames,m_nFrames);
+      if (out_adv < in_adv) // decreasing channel count, left to right
+      {
+        while (n--)
+        {
+          if (out!=in) memmove(out,in,copysz);
+          out+=out_adv;
+          in+=in_adv;
+        }
+      }
+      else // increasing channel count, copy right to left
+      {
+        out += n * out_adv;
+        in += n * in_adv;
+        while (n--)
+        {
+          out-=out_adv;
+          in-=in_adv;
+          if (out!=in) memmove(out,in,copysz);
+        }
+      }
+      // adjust interleaving
+    }
   }
   
   m_data.Resize(newsz);
