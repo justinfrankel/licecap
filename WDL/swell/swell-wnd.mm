justinfrankel/licecap

commit 0e17731077753e7b7c4f62f41c1a4ed4e6e69fd4

SWELL: improved NCHITTEST compat with scrollbars -- from 870a8ed1

WDL/swell/swell-wnd.mm
@@ -4840,7 +4840,16 @@ LRESULT DefWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
   }
   else if (msg==WM_NCHITTEST) 
   {
-    return HTCLIENT;
+    int rv=HTCLIENT;
+    SWELL_BEGIN_TRY
+    RECT r;
+    GetWindowRect(hwnd,&r);
+    if (r.top > r.bottom) { int a=r.top; r.top=r.bottom; r.bottom=a; }
+    SendMessage(hwnd,WM_NCCALCSIZE,FALSE,(LPARAM)&r);
+    POINT pt={GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam)};
+    if (!PtInRect(&r,pt)) rv=HTNOWHERE;
+    SWELL_END_TRY(;)
+    return rv;
   }
   else if (msg==WM_KEYDOWN || msg==WM_KEYUP) return 69;
   else if (msg == WM_DISPLAYCHANGE)

WDL/wingui/scrollbar/coolscroll.cpp
@@ -2082,19 +2082,6 @@ static LRESULT NCHitTest(SCROLLWND *sw, HWND hwnd, WPARAM wParam, LPARAM lParam)
     vrect.bottom += zbs*2+ZOOMBUTTON_RESIZER_SIZE(zbs);
   }
 	  
-#ifndef _WIN32
-  if (sw->sbarHorz.fScrollVisible || sw->sbarVert.fScrollVisible)
-  {
-    int sz=sw->sbarHorz.fScrollVisible ? hrect.bottom-hrect.top : vrect.right-vrect.left;
-    if (sz<0)sz=-sz;
-    RECT r;
-    GetWindowRect(hwnd,&r);
-    int bot = max(r.top,r.bottom);
-    int v= (bot-pt.y) + (r.right-pt.x);
-    if (v < sz*3/4) return HTNOWHERE;
-  }
-#endif
-
     
 	//Clicked in the horizontal scrollbar area
 	if(sw->sbarHorz.fScrollVisible && PtInRect(&hrect, pt))
@@ -2107,12 +2094,6 @@ static LRESULT NCHitTest(SCROLLWND *sw, HWND hwnd, WPARAM wParam, LPARAM lParam)
 
 		return HTVSCROLL;
 	}
-#ifndef _WIN32
-  else if (sw->sbarHorz.fScrollVisible && sw->sbarVert.fScrollVisible && pt.y >= hrect.top && pt.y <= hrect.bottom && pt.x >= vrect.left && pt.x <= vrect.right)
-  {
-    return HTNOWHERE;
-  }
-#endif  
 	//clicked somewhere else
 	else
 	{

commit ef6e78339565633747f266b20e946480e98d44fc

SWELL: fixed mouse hit testing broken in 0e1773107 -- from 18196d6f

WDL/swell/swell-wnd.mm
@@ -4844,10 +4844,16 @@ LRESULT DefWindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
     SWELL_BEGIN_TRY
     RECT r;
     GetWindowRect(hwnd,&r);
-    if (r.top > r.bottom) { int a=r.top; r.top=r.bottom; r.bottom=a; }
+    POINT pt={GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam)};
+
+    if (r.top > r.bottom) 
+    { 
+      pt.y = r.bottom + (r.top - pt.y); // translate coordinate into flipped-window
+
+      int a=r.top; r.top=r.bottom; r.bottom=a; 
+    }
     NCCALCSIZE_PARAMS p={{r,}};
     SendMessage(hwnd,WM_NCCALCSIZE,FALSE,(LPARAM)&p);
-    POINT pt={GET_X_LPARAM(lParam),GET_Y_LPARAM(lParam)};
     if (!PtInRect(&p.rgrc[0],pt)) rv=HTNOWHERE;
     SWELL_END_TRY(;)
     return rv;