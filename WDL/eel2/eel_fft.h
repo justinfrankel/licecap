fdf8bc21dcaae03e4b98760abff83503c1c9cef8
Cockos Inc <nospam@cockos.com>
2013-12-31 23:19:48 -0500

    eel_fft: faster fft_permute() functions -- from f59db15e

--- WDL/eel2/eel_fft.h
+++ WDL/eel2/eel_fft.h
@@ -15,6 +15,94 @@
 #endif
 
 
+#ifndef EEL_SLOW_FFT_REORDERING
+
+static int *fft_reorder_table_for_bitsize(int bitsz)
+{
+  static int s_tab[ (2 << EEL_FFT_MAXBITLEN) + 24*(EEL_FFT_MAXBITLEN-EEL_FFT_MINBITLEN+1) ];
+  if (bitsz<=EEL_FFT_MINBITLEN) return s_tab;
+  return s_tab + (1<<bitsz) + (bitsz-EEL_FFT_MINBITLEN) * 24;
+}
+static void fft_make_reorder_table(int bitsz, int *tab)
+{
+  const int fft_sz=1<<bitsz;
+  char flag[1<<EEL_FFT_MAXBITLEN];
+  int x;
+  int *tabstart = tab;
+  memset(flag,0,fft_sz);
+
+  for (x=0;x<fft_sz;x++)
+  {
+    int fx;
+    if (!flag[x] && (fx=WDL_fft_permute(fft_sz,x))!=x)
+    {
+      flag[x]=1;
+      *tab++ = x*2;
+      do
+      {
+        flag[fx]=1;
+        *tab++ = fx*2;
+        fx = WDL_fft_permute(fft_sz, fx);
+      }
+      while (fx != x);
+      *tab++ = 0; // delimit a run
+    }
+    else flag[x]=1;
+  }
+  *tab++ = 0; // doublenull terminated
+}
+
+static void fft_reorder_buffer(int bitsz, EEL_F *data, int fwd)
+{
+  const int *tab=fft_reorder_table_for_bitsize(bitsz);
+  if (!fwd)
+  {
+    while (*tab)
+    {
+      const int sidx=*tab++;
+      EEL_F a=data[sidx];
+      EEL_F b=data[sidx+1];
+      for (;;)
+      {
+        EEL_F ta,tb;
+        const int idx=*tab++;
+        if (!idx) break;
+        ta=data[idx];
+        tb=data[idx+1];
+        data[idx]=a;
+        data[idx+1]=b;
+        a=ta;
+        b=tb;
+      }
+      data[sidx] = a;
+      data[sidx+1] = b;
+    }
+  }
+  else
+  {
+    while (*tab)
+    {
+      const int sidx=*tab++;
+      int lidx = sidx;
+      const EEL_F sta=data[lidx];
+      const EEL_F stb=data[lidx+1];
+      for (;;)
+      {
+        const int idx=*tab++;
+        if (!idx) break;
+
+        data[lidx]=data[idx];
+        data[lidx+1]=data[idx+1];
+        lidx=idx;
+      }
+      data[lidx] = sta;
+      data[lidx+1] = stb;
+    }
+  }
+}
+
+#endif
+
 // 0=fw, 1=iv, 2=fwreal, 3=ireal, 4=permutec, 6=permuter
 // low bit: is inverse
 // second bit: is real
@@ -28,8 +116,12 @@ static void FFT(int sizebits, EEL_F *data, int dir)
     int x;
     if (dir == 4 || dir == 5)
     {
+#ifndef EEL_SLOW_FFT_REORDERING
+      // this in-place reordering is about 2x as fast, probably because of both cache effects as well as alloca() being used
+      fft_reorder_buffer(sizebits,data,dir==4);
+#else
       EEL_F *tmp=(EEL_F*)alloca(sizeof(EEL_F)*flen*2);
-    	int flen2=flen+flen;
+    	const int flen2=flen+flen;
 	    // reorder entries, now
       memcpy(tmp,data,sizeof(EEL_F)*flen*2);
 
@@ -41,6 +133,7 @@ static void FFT(int sizebits, EEL_F *data, int dir)
           data[x]=tmp[y];
    	      data[x+1]=tmp[y+1];
         }
+
       }
       else
       {
@@ -51,6 +144,7 @@ static void FFT(int sizebits, EEL_F *data, int dir)
    	      data[y+1]=tmp[x+1];
         }
       }
+#endif
     }
   }
 	else if (dir >= 0 && dir < 2)
@@ -141,11 +235,16 @@ static EEL_F * NSEEL_CGEN_CALL eel_convolve_c(EEL_F **blocks,EEL_F *dest, EEL_F 
   return dest;
 }
 
-
-
 static void EEL_fft_register()
 {
   WDL_fft_init();
+#ifndef EEL_SLOW_FFT_REORDERING
+  if (!fft_reorder_table_for_bitsize(EEL_FFT_MINBITLEN)[0])
+  {
+    int x;
+    for (x=EEL_FFT_MINBITLEN;x<=EEL_FFT_MAXBITLEN;x++) fft_make_reorder_table(x,fft_reorder_table_for_bitsize(x));
+  }
+#endif
   NSEEL_addfunctionex("convolve_c",3,(char *)_asm_generic3parm,(char *)_asm_generic3parm_end-(char *)_asm_generic3parm,NSEEL_PProc_RAM,(void*)&eel_convolve_c);
 
   NSEEL_addfunctionex("fft",2,(char *)_asm_generic2parm,(char *)_asm_generic2parm_end-(char *)_asm_generic2parm,NSEEL_PProc_RAM,(void*)eel_fft);
