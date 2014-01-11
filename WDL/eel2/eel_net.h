25fee8da17b749da9772e88b73c57737d9ea9fb8
Cockos Inc <nospam@cockos.com>
2014-01-11 12:49:36 -0500

    eel_net: listen support eel: test app has a makefile.vc6 -- from 9abd1391

--- WDL/eel2/eel_net.h
+++ WDL/eel2/eel_net.h
@@ -1,7 +1,7 @@
 #ifndef _EEL_NET_H_
 #define _EEL_NET_H_
 
-// x = tcp_listen(port) poll this, returns connection id  > 0
+// x = tcp_listen(port[,interface, connected_ip_out]) poll this, returns connection id  > 0, or <0 on error, or 0 if no new connect -- interface only valid on first call (or after tcp_listen_end(port))
 // tcp_listen_end(port); 
 
 // connection = tcp_connect(host, port[, block]) // connection id > 0 on ok
@@ -62,6 +62,7 @@ class eel_net_state
     EEL_F onConnect(char *hostNameOwned, int port, int block);
     EEL_F onClose(EEL_F handle);
     EEL_F set_block(EEL_F handle, bool block);
+    EEL_F onListen(void *opaque, EEL_F handle, int mode, EEL_F *ifStr, EEL_F *ipOut);
 
     int __run_connect(connection_state *cs, unsigned int ip);
     int __run(connection_state *cs);
@@ -146,6 +147,88 @@ EEL_F eel_net_state::onConnect(char *hostNameOwned, int port, int block)
   }
   free(hostNameOwned);
   return -1;
+}
+
+EEL_F eel_net_state::onListen(void *opaque, EEL_F handle, int mode, EEL_F *ifStr, EEL_F *ipOut)
+{
+  const int port = (int) handle;
+  if (port < 1 || port > 65535) return 0.0;
+  SOCKET *s = m_listens.GetPtr(port);
+  if (mode<0)
+  {
+    if (!s) return -1.0;
+
+    SOCKET ss=*s;
+    m_listens.Delete(port);
+    if (ss != INVALID_SOCKET) closesocket(ss);
+    return 0.0;
+  }
+  if (!s)
+  {
+    struct sockaddr_in sin;
+    memset((char *) &sin, 0,sizeof(sin));
+    if (ifStr)
+    {
+      EEL_STRING_MUTEXLOCK_SCOPE
+      const char *fn = EEL_STRING_GET_FOR_INDEX(*ifStr,NULL);
+      if (fn && *fn) sin.sin_addr.s_addr=inet_addr(fn);
+    }
+    if (!sin.sin_addr.s_addr || sin.sin_addr.s_addr==INADDR_NONE) sin.sin_addr.s_addr = INADDR_ANY;
+    sin.sin_family = AF_INET;
+    sin.sin_port = htons( (short) port );
+    SOCKET sock = socket(AF_INET,SOCK_STREAM,0);
+    if (sock != INVALID_SOCKET)
+    {
+      SET_SOCK_BLOCK(sock,0);
+      if (bind(sock,(struct sockaddr *)&sin,sizeof(sin)) || listen(sock,8)==-1) 
+      {
+        closesocket(sock);
+        sock=INVALID_SOCKET;
+      }
+    }
+    m_listens.Insert(port,sock);
+    s = m_listens.GetPtr(port);
+  }
+  if (!s || *s == INVALID_SOCKET) return -1;
+
+  struct sockaddr_in saddr;
+  socklen_t length = sizeof(struct sockaddr_in);
+  SOCKET newsock = accept(*s, (struct sockaddr *) &saddr, &length);
+  if (newsock == INVALID_SOCKET) 
+  {
+    return 0; // nothing to report here
+  }
+
+  int x;
+  for(x=0;x<m_cons.GetSize();x++)
+  {
+    connection_state *s=m_cons.Get()+x;
+    if (s->state == STATE_FREE)
+    {
+      s->state=STATE_CONNECTED;
+      free(s->hostname);
+      s->hostname=NULL;
+      s->sock = newsock;
+      s->blockmode=0;
+      s->port=0;
+      if (ipOut)
+      {
+        EEL_STRING_MUTEXLOCK_SCOPE
+        WDL_FastString *ws=NULL;
+        EEL_STRING_GET_FOR_INDEX(*ipOut,&ws);
+        if (ws)
+        {
+          const unsigned int a = ntohl(saddr.sin_addr.s_addr);
+          ws->SetFormatted(128,"%d.%d.%d.%d",(a>>24)&0xff,(a>>16)&0xff,(a>>8)&0xff,a&0xff);
+        }
+      }
+      return x + CONNECTION_ID_BASE;
+    }
+  }
+  closesocket(newsock);
+  return -1;
+
+
 }
 
 int eel_net_state::__run_connect(connection_state *cs, unsigned int ip)
@@ -345,6 +428,20 @@ static EEL_F NSEEL_CGEN_CALL _eel_tcp_send(void *opaque, INT_PTR np, EEL_F **par
   return -1;
 }
 
+static EEL_F NSEEL_CGEN_CALL _eel_tcp_listen(void *opaque, INT_PTR np, EEL_F **parms)
+{
+  eel_net_state *ctx;
+  if (NULL != (ctx=EEL_NET_GET_CONTEXT(opaque))) return ctx->onListen(opaque,parms[0][0],1,np>1?parms[1]:NULL,np>2?parms[2]:NULL);
+  return 0;
+}
+
+static EEL_F NSEEL_CGEN_CALL _eel_tcp_listen_end(void *opaque, EEL_F *handle)
+{
+  eel_net_state *ctx;
+  if (NULL != (ctx=EEL_NET_GET_CONTEXT(opaque))) return ctx->onListen(opaque,*handle,-1,NULL,NULL);
+  return 0;
+}
+
 void EEL_tcp_initsocketlib() // call per thread
 {
 #ifdef _WIN32
@@ -355,10 +452,8 @@ void EEL_tcp_initsocketlib() // call per thread
 
 void EEL_tcp_register()
 {
-/*
-  NSEEL_addfunctionex("tcp_listen",1,(char *)_asm_generic1parm_retd,(char *)_asm_generic1parm_retd_end-(char *)_asm_generic1parm_retd,NSEEL_PProc_THIS,(void *)&_eel_tcp_listen);
+  NSEEL_addfunc_varparm("tcp_listen",1,NSEEL_PProc_THIS,(void *)&_eel_tcp_listen);
   NSEEL_addfunctionex("tcp_listen_end",1,(char *)_asm_generic1parm_retd,(char *)_asm_generic1parm_retd_end-(char *)_asm_generic1parm_retd,NSEEL_PProc_THIS,(void *)&_eel_tcp_listen_end);
-  */
 
   NSEEL_addfunc_varparm("tcp_connect",2,NSEEL_PProc_THIS,(void *)&_eel_tcp_connect);
   NSEEL_addfunc_varparm("tcp_send",2,NSEEL_PProc_THIS,(void *)&_eel_tcp_send);
