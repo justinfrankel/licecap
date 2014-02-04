2cde99f28f5b598b4395c37db606751afaf88b8d
Cockos Inc <nospam@cockos.com>
2014-02-03 20:12:38 -0500

    libpng: include ../zlib/zlib.h -- from c4e5e9b0

--- WDL/libpng/pngstruct.h
+++ WDL/libpng/pngstruct.h
@@ -28,7 +28,7 @@
    /* We must ensure that zlib uses 'const' in declarations. */
 #  define ZLIB_CONST
 #endif
-#include "zlib.h"
+#include "../zlib/zlib.h"
 #ifdef const
    /* zlib.h sometimes #defines const to nothing, undo this. */
 #  undef const
