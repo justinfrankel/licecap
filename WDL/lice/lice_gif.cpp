fc73518092941d5438c28c8533f68241c7724f78
Cockos Inc <nospam@cockos.com>
2014-08-05 11:46:00 -0400

    LICE: gif loading properly handles transparency, interlaced images, and multiple images, and animation as a vertical stack -- from bae85d9c

--- WDL/lice/lice_gif.cpp
+++ WDL/lice/lice_gif.cpp
@@ -6,6 +6,7 @@
 */
 
 #include "lice.h"
+#include "../heapbuf.h"
 #include <stdio.h>
 
 extern "C" {
@@ -14,7 +15,107 @@ extern "C" {
 int _GifError;
 };
 
+static void applyGifFrameToBitmap(LICE_IBitmap *bmp, GifFileType *fp, int transparent_pix, bool clear)
+{
+  const int width=fp->Image.Width,height=fp->Image.Height;
+
+  LICE_pixel cmap[256];
+  GifPixelType _linebuf[2048];
+  GifPixelType *linebuf=width > 2048 ? (GifPixelType*)malloc(width*sizeof(GifPixelType)) : _linebuf;
+  int y;
+  y=0;
+  if (fp->Image.ColorMap && fp->Image.ColorMap->Colors) for (;y<256 && y < fp->Image.ColorMap->ColorCount;y++)
+  {
+    GifColorType *ct=fp->Image.ColorMap->Colors+y;
+    cmap[y]=LICE_RGBA(ct->Red,ct->Green,ct->Blue,255);
+  }
+
+  if (fp->SColorMap && fp->SColorMap->Colors) for (;y<256 && y < fp->SColorMap->ColorCount;y++)
+  {
+    GifColorType *ct=fp->SColorMap->Colors+y;
+    cmap[y]=LICE_RGBA(ct->Red,ct->Green,ct->Blue,255);
+  }
+  for (;y<256;y++) cmap[y]=0;
+
+  if (clear)
+  {
+    LICE_pixel col = 0;
+
+    if (fp->SColorMap && fp->SColorMap->Colors && fp->SBackGroundColor >= 0 && fp->SBackGroundColor < fp->SColorMap->ColorCount)
+    {
+      GifColorType *ct=fp->SColorMap->Colors+fp->SBackGroundColor;
+      col = LICE_RGBA(ct->Red,ct->Green,ct->Blue,0);
+    }
+    else if (fp->SBackGroundColor>=0 && fp->SBackGroundColor<256)
+    {
+      col = cmap[fp->SBackGroundColor] & LICE_RGBA(255,255,255,0);
+    }
+    LICE_Clear(bmp,col);
+  }
+
+  const int bmp_h = bmp->getHeight();
+  const int bmp_w = bmp->getWidth();
+  LICE_pixel *bmp_ptr = bmp->getBits();
+  int bmp_span = bmp->getRowSpan();
+  if (bmp->isFlipped())
+  {
+    bmp_ptr += (bmp_h-1)*bmp_span;
+    bmp_span = -bmp_span;
+  }
+  const int xpos = fp->Image.Left;
+
+  int skip=0;
+  int use_width = width;
+  if (xpos < 0) skip = -xpos;
+
+  if (use_width > bmp_w - xpos) use_width = bmp_w - xpos;
+
+  int ystate = 0, ypass=0;
+  for (y=0; y < height; y ++)
+  {
+    if (DGifGetLine(fp,linebuf,width)==GIF_ERROR) break;
+
+    int ypos;
+    if (fp->Image.Interlace)
+    {
+      if (ypass == 0)
+      {
+        ypos = ystate++ * 8;
+        if (ypos >= height) { ypass++; ystate=0; }
+      }
+      if (ypass == 1)
+      {
+        ypos = ystate++ * 8 + 4;
+        if (ypos >= height) { ypass++; ystate=0; }
+      }
+      if (ypass == 2)
+      {
+        ypos = ystate++ * 4 + 2;
+        if (ypos >= height) { ypass++; ystate=0; }
+      }
+      if (ypass==3) ypos = ystate++ * 2 + 1;
+    }
+    else
+    {
+      ypos = ystate++;
+    }
+
+    ypos += fp->Image.Top;
+    if (ypos < 0 || ypos >= bmp_h) continue;
+
+    LICE_pixel *p = bmp_ptr + ypos*bmp_span + xpos;
+    int x;
+    for (x = skip; x < use_width; x ++) 
+    {
+      const int a = (int) linebuf[x];
+      if (a != transparent_pix) p[x]=cmap[a];
+    }
+  }
+  if (linebuf != _linebuf) free(linebuf);
+}
+
 static int readfunc_fh(GifFileType *fh, GifByteType *buf, int sz) { return (int)fread(buf, 1, sz, (FILE *)fh->UserData); }
+
 
 LICE_IBitmap *LICE_LoadGIF(const char *filename, LICE_IBitmap *bmp, int *nframes)
 {
@@ -27,9 +128,10 @@ LICE_IBitmap *LICE_LoadGIF(const char *filename, LICE_IBitmap *bmp, int *nframes
       fpp = _wfopen(wf,L"rb");
   }
 #endif
+  if (nframes) *nframes=0;
 
   if (!fpp) fpp = fopen(filename,"rb");
-  if(!fpp) return 0;
+  if (!fpp) return 0;
 
   GifFileType *fp=DGifOpen(fpp, readfunc_fh);
   if (!fp)
@@ -38,108 +140,149 @@ LICE_IBitmap *LICE_LoadGIF(const char *filename, LICE_IBitmap *bmp, int *nframes
     return 0;
   }
   
+  int transparent_pix = -1;
+  
+  bool had_image = false;
+  bool had_delay = false;
+  bool need_new_frame = false;
+
+  const bool own_bmp = !bmp;
   GifRecordType RecordType;
   GifByteType *Extension;
   int ExtCode;
-  do 
+  LICE_WrapperBitmap workbm(NULL,0,0,0,false);
+  WDL_HeapBuf workbuf(128*1024);
+  
+
+  do
   {
     if (DGifGetRecordType(fp, &RecordType) == GIF_ERROR) break;
 
-  	switch (RecordType) 
+    switch (RecordType)
     {
-	    case IMAGE_DESC_RECORD_TYPE:
-		    if (DGifGetImageDesc(fp) == GIF_ERROR) 
+      case IMAGE_DESC_RECORD_TYPE:
+        if (DGifGetImageDesc(fp) == GIF_ERROR)
         {
           RecordType = TERMINATE_RECORD_TYPE;
           break;
-  		  }
+        }
 
-        // todo: support transparency
-        // todo: have it buffer all frames and output frame count
-        if (nframes) *nframes=1; // placeholder 
+        if (!bmp) bmp=new LICE_MemBitmap;
 
+        if (!had_image || (nframes && need_new_frame))
         {
-          int width=fp->Image.Width,height=fp->Image.Height;
-          if (bmp)
+          if (had_image)
           {
-            bmp->resize(width,height);
-            if (bmp->getWidth() != (int)width || bmp->getHeight() != (int)height) 
+            // implies nframes
+            const int ht = (*nframes+1) * (int)fp->SHeight;
+            workbm.m_h = ht;
+            workbm.m_w = (int)fp->SWidth;
+            workbm.m_span = (workbm.m_w+3)&~3;
+            workbm.m_buf = (LICE_pixel *) workbuf.ResizeOK(ht * sizeof(LICE_pixel) * workbm.m_span);
+            if (!workbm.m_buf)
             {
               RecordType = TERMINATE_RECORD_TYPE;
               break;
             }
           }
-          else bmp=new LICE_MemBitmap(width,height);
+          else
+          {
+            bmp->resize(fp->SWidth,fp->SHeight);
 
-          LICE_pixel *bmpptr = bmp->getBits();
-          int dbmpptr=bmp->getRowSpan();
-          if (bmp->isFlipped())
-          {
-            bmpptr += dbmpptr*(bmp->getHeight()-1);
-            dbmpptr=-dbmpptr;
-          }
-          GifPixelType *linebuf=(GifPixelType*)malloc(width*sizeof(GifPixelType));
-          int y;
-          int cmap[256];
-          for (y=0;y<256;y++)
-          {
-            if (fp->SColorMap&&y<fp->SColorMap->ColorCount&&fp->SColorMap->Colors)
+            if (bmp->getWidth() != (int)fp->SWidth || bmp->getHeight() != (int)fp->SHeight)
             {
-              GifColorType *ct=fp->SColorMap->Colors+y;
-              cmap[y]=LICE_RGBA(ct->Red,ct->Green,ct->Blue,255);
+              RecordType = TERMINATE_RECORD_TYPE;
+              break;
             }
-            else cmap[y]=0;
           }
-
-          for (y=0; y < height; y ++)
+          
+          if (nframes)
           {
-            if (DGifGetLine(fp,linebuf,width)==GIF_ERROR) break;
-
-            int x;
-            for (x = 0; x < width; x ++)
+            if (*nframes > 0)
             {
-              bmpptr[x]=cmap[linebuf[x]&0xff];
+              if (*nframes == 1)
+                LICE_Blit(&workbm,bmp,0,0,0,0,bmp->getWidth(),bmp->getHeight(),1.0,LICE_BLIT_MODE_COPY);
+            
+              LICE_Blit(&workbm,&workbm,0,workbm.getHeight()-(int)fp->SHeight,0,
+                        workbm.getHeight()-(int)fp->SHeight*2, workbm.getWidth(), (int)fp->SHeight,1.0f,LICE_BLIT_MODE_COPY);
             }
-            bmpptr += dbmpptr;
+            
+            *nframes += 1;
           }
-
-
-
-          free(linebuf);
-          DGifCloseFile(fp);
-          fclose(fpp);
-          return bmp;
         }
-  		break;
-	    case EXTENSION_RECORD_TYPE:
-    		if (DGifGetExtension(fp, &ExtCode, &Extension) == GIF_ERROR) 
+
+        if (nframes)
+        {
+          LICE_SubBitmap tmp(&workbm, 0,workbm.getHeight() - (int) fp->SHeight, workbm.getWidth(), (int)fp->SHeight);
+          applyGifFrameToBitmap(&tmp,fp,transparent_pix,!had_image);
+        }
+        else
+          applyGifFrameToBitmap(bmp,fp,transparent_pix,!had_image);
+
+        had_image = true;
+
+        need_new_frame = false;
+        transparent_pix = -1;
+
+        if (had_delay) 
+        {
+          if (!nframes)
+          {
+            RecordType = TERMINATE_RECORD_TYPE; // finish up if first frame is finished
+          }
+          else
+          {
+            need_new_frame = true;
+          }
+          had_delay = false;
+        }
+      break;
+      case EXTENSION_RECORD_TYPE:
+        if (DGifGetExtension(fp, &ExtCode, &Extension) == GIF_ERROR) 
         {
           RecordType = TERMINATE_RECORD_TYPE;
         }
         else
         {
-    		  while (Extension != NULL) 
+          while (Extension != NULL) 
           {
-		        if (DGifGetExtensionNext(fp, &Extension) == GIF_ERROR) 
+            if (ExtCode == 0xF9 && *Extension >= 4)
+            {
+              transparent_pix = -1;
+              if (Extension[1]&1)
+              {
+                transparent_pix = Extension[4];
+              }
+              if (Extension[2] || Extension[3]) had_delay=true;
+            }
+
+            if (DGifGetExtensionNext(fp, &Extension) == GIF_ERROR) 
             {
               RecordType = TERMINATE_RECORD_TYPE;
               break;
-  		      }
-		      }
+            }
+          }
         }
       break;
-	    case TERMINATE_RECORD_TYPE:
-  		break;
-	    default:		    /* Should be traps by DGifGetRecordType. */
-	  	break;
-	  }
+      case TERMINATE_RECORD_TYPE:
+      break;
+      default: /* Should be traps by DGifGetRecordType. */
+      break;
+    }
   }
   while (RecordType != TERMINATE_RECORD_TYPE);
 
   DGifCloseFile(fp);
   fclose(fpp);
-  return 0;
-
+  
+  if (had_image)
+  {
+    if (workbm.getWidth()) LICE_Copy(bmp,&workbm);
+    return bmp;
+  }
+  
+  if (own_bmp) delete bmp;
+  return NULL;
 }
 
 
