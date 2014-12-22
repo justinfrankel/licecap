d6156c97c762331799cac524c5db1f2bbfb4a717
Justin Frankel <justin@cockos.com>
2014-12-21 19:47:04 -0500

    Revert "disabled WDL_NEW by default, since older xcode doesnt seem to jive with it"

--- WDL/wdltypes.h
+++ WDL/wdltypes.h
@@ -79,7 +79,7 @@ typedef bool WDL_bool;
   #define WDL_STATICFUNC_UNUSED
 #endif
 
-#ifdef WDL_WANT_NOTHROW // isn't yet supported everywhere we want it to be
+#ifndef WDL_WANT_NEW_EXCEPTIONS
 #if defined(__cplusplus)
 #include <new>
 #define WDL_NEW (std::nothrow)
