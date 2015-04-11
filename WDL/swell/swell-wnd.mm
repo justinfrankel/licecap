58566b2a3de19f9d96bf77072f436ae8a6e42f75
Cockos Inc <nospam@cockos.com>
2015-04-11 18:15:43 +0200

    SWELL_MakeControl make use of LVCF_TEXT|, LVCF_WIDTH -- from de8ad4fb

--- WDL/swell/swell-wnd.mm
+++ WDL/swell/swell-wnd.mm
@@ -3334,6 +3334,7 @@ HWND SWELL_MakeControl(const char *cname, int idx, const char *classname, int st
     if (isLB || !(style & LVS_REPORT))
     {
       LVCOLUMN lvc={0,};
+      lvc.mask=LVCF_TEXT|LVCF_WIDTH;
       lvc.cx=(int)ceil(wdl_max(tr.size.width,300.0));
       lvc.pszText=(char*)"";
       ListView_InsertColumn((HWND)obj,0,&lvc);
