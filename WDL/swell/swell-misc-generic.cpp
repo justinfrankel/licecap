3f34f614db9240f76ed5bb699fa7c062c343873b
Justin Frankel <justin@cockos.com>
2015-07-05 11:50:30 -0400

    swell: swell-misc-generic matching upstream

--- WDL/swell/swell-misc-generic.cpp
+++ WDL/swell/swell-misc-generic.cpp
@@ -3,10 +3,6 @@
 #include "swell.h"
 #include "swell-internal.h"
 
-HANDLE SWELL_CreateProcess(const char *exe, int nparams, const char **params)
-{
-  return 0; // todo
-}
 bool IsRightClickEmulateEnabled()
 {
   return false;
@@ -14,6 +10,10 @@ bool IsRightClickEmulateEnabled()
 
 void SWELL_EnableRightClickEmulate(BOOL enable)
 {
+}
+HANDLE SWELL_CreateProcess(const char *exe, int nparams, const char **params)
+{
+  return 0; // todo
 }
 
 
