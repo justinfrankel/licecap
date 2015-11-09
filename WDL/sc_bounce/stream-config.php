158459b570a6b2e3b5e1ca5577db9d055d349631
Cockos Inc <nospam@cockos.com>
2015-11-09 15:49:15 +0000

    sc_bounce: modernize implementation, add new persistence capabilities -- from c252285e

--- WDL/sc_bounce/stream-config.php
+++ WDL/sc_bounce/stream-config.php
@@ -1,21 +1,21 @@
-<?
+<?php
 
-// some path that you will be able to create
-$config_temp_dir = "/tmp/username_stream_data"; 
-
+// some path that the server can write in (or create)
 $config_default_stream = "test"; // if no stream specified, use this one
+$config_session_timeout = 120; // seconds of inactivity = new session 
+$config_presend = 32000; // presend bytes on listener connect
+$config_keep_files = true; // keep old recordings
 
-
-// allowed servers, with their name, password, optional listen password
+// allowed servers, with their name, password, write path, optional listen password
 $config_streams = array(
-  "test" => array("password" => "somePassword", "listen_password" => "",),
+  "test" => array("password" => "somePassword", 
+                  "path" => "test_files",   // optional (stream name used if not specified)
+                  "session_timeout" => 120, // timeout in seconds ($config_session_timeout)
+                  "presend" => 32000,       // pre-send bytes ($config_presend)
+                  "keep_files" => true,
+                  "listen_password" => "",),
 
 );
 
-
-// performance tweaks ( safe to leave these as is for now )
-
-$config_num_files = 4; 
-$config_file_timeout = 30;
 
 ?>
