de2258e325e3171ff7d983fe565cf84744d66b45
Cockos Inc <nospam@cockos.com>
2016-05-02 10:28:17 -0400

    wdlutf8: use WDL_STATICFUNC_UNUSED -- from 677d458a, 3bfe0d73

--- WDL/wdlutf8.h
+++ WDL/wdlutf8.h
@@ -27,6 +27,8 @@ misrepresented as being the original software.
  * todo: handle multi-byte (make WideStr support UTF-16)
  */
 
+#include "wdltypes.h"
+
 #ifndef WDL_WCHAR
   #ifdef _WIN32
     #define WDL_WCHAR WCHAR
@@ -37,7 +39,7 @@ misrepresented as being the original software.
 
 
 // returns size, sets bytesUsed. If invalid UTF-8, returns first character (as unsigned)
-static int wdl_utf8_parsechar(const char *rd, int *cOut) 
+static int WDL_STATICFUNC_UNUSED wdl_utf8_parsechar(const char *rd, int *cOut) 
 {
   const unsigned char *p = (const unsigned char *)rd;
   const unsigned char b0 = *p;
@@ -94,7 +96,7 @@ static int wdl_utf8_parsechar(const char *rd, int *cOut)
 
 // makes a character, returns length. does NOT nul terminate.
 // returns 0 if insufficient space, -1 if out of range value
-static int wdl_utf8_makechar(int c, char *dest, int dest_len)
+static int WDL_STATICFUNC_UNUSED wdl_utf8_makechar(int c, char *dest, int dest_len)
 {
   if (c < 0) return -1; // out of range character
 
@@ -136,7 +138,7 @@ static int wdl_utf8_makechar(int c, char *dest, int dest_len)
 
 
 // invalid UTF-8 are now treated as ANSI characters for this function
-static int WDL_MBtoWideStr(WDL_WCHAR *dest, const char *src, int destlenbytes)
+static int WDL_STATICFUNC_UNUSED WDL_MBtoWideStr(WDL_WCHAR *dest, const char *src, int destlenbytes)
 {
   WDL_WCHAR *w = dest, *dest_endp = dest+(size_t)destlenbytes/sizeof(WDL_WCHAR)-1;
   if (!dest || destlenbytes < 1) return 0;
@@ -154,7 +156,7 @@ static int WDL_MBtoWideStr(WDL_WCHAR *dest, const char *src, int destlenbytes)
 
 // like wdl_utf8_makechar, except nul terminates and handles errors differently (returns _ and 1 on errors)
 // negative values for character are treated as 0.
-static int WDL_MakeUTFChar(char* dest, int c, int destlen)
+static int WDL_STATICFUNC_UNUSED WDL_MakeUTFChar(char* dest, int c, int destlen)
 {
   if (destlen < 2)
   {
@@ -175,7 +177,7 @@ static int WDL_MakeUTFChar(char* dest, int c, int destlen)
   }
 }
 
-static int WDL_WideToMBStr(char *dest, const WDL_WCHAR *src, int destlenbytes)
+static int WDL_STATICFUNC_UNUSED WDL_WideToMBStr(char *dest, const WDL_WCHAR *src, int destlenbytes)
 {
   char *p = dest, *dest_endp = dest + destlenbytes - 1;
   if (!dest || destlenbytes < 1) return 0;
@@ -194,7 +196,7 @@ static int WDL_WideToMBStr(char *dest, const WDL_WCHAR *src, int destlenbytes)
 }
 
 // returns >0 if UTF-8, -1 if 8-bit chars occur that are not UTF-8, or 0 if ASCII
-static int WDL_DetectUTF8(const char *str)
+static int WDL_STATICFUNC_UNUSED WDL_DetectUTF8(const char *str)
 {
   int hasUTF=0;
 
@@ -221,7 +223,7 @@ static int WDL_DetectUTF8(const char *str)
 }
 
 
-static int WDL_utf8_charpos_to_bytepos(const char *str, int charpos)
+static int WDL_STATICFUNC_UNUSED WDL_utf8_charpos_to_bytepos(const char *str, int charpos)
 {
   int bpos = 0;
   while (charpos-- > 0 && str[bpos])
@@ -230,7 +232,7 @@ static int WDL_utf8_charpos_to_bytepos(const char *str, int charpos)
   }
   return bpos;
 }
-static int WDL_utf8_bytepos_to_charpos(const char *str, int bytepos)
+static int WDL_STATICFUNC_UNUSED WDL_utf8_bytepos_to_charpos(const char *str, int bytepos)
 {
   int bpos = 0, cpos=0;
   while (bpos < bytepos && str[bpos])
