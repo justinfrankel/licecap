9afc950eecd7145e81df3aaf2171bed87315137f
Cockos Inc <nospam@cockos.com>
2016-09-10 17:52:10 -0400

    fft.c: const nitpicks -- from d8e172ff

--- WDL/fft.c
+++ WDL/fft.c
@@ -1198,8 +1198,8 @@ static inline void v2(register WDL_FFT_REAL *a)
 
 static void two_for_one(WDL_FFT_REAL* buf, const WDL_FFT_COMPLEX *d, int len, int isInverse)
 {
-  unsigned int half = (unsigned)len >> 1, quart = half >> 1, eighth = quart >> 1;
-  int* permute = WDL_fft_permute_tab(half);
+  const unsigned int half = (unsigned)len >> 1, quart = half >> 1, eighth = quart >> 1;
+  const int *permute = WDL_fft_permute_tab(half);
   unsigned int i, j;
 
   WDL_FFT_COMPLEX *p, *q, tw, sum, diff;
