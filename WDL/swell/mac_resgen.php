2ecb43d86c689e1afad4eb11daa7b67cda04784b
Cockos Inc <nospam@cockos.com>
2017-02-14 12:18:51 -0500

    SWELL: mac_resgen.php supports autoconverting UTF-16 input -- from 2edcf4fe

--- WDL/swell/mac_resgen.php
+++ WDL/swell/mac_resgen.php
@@ -40,9 +40,44 @@ function convertquotes($in)
   }
   return $ret;
 }
+
+function utf_fgets($fp, &$state)
+{
+  if ($state==0)
+  {
+    $state = -1;
+    $s = fread($fp,2);
+    if (ord($s[0])==0xff && ord($s[1]) == 0xfe) $state=1; // UTF-16LE
+    else if (ord($s[0])==0xfe && ord($s[1]) == 0xff) $state=2; // UTF-16BE
+    else if (ord($s[0])==0xef && ord($s[1]) == 0xbb)
+    {
+      $s2 = fread($fp,1);
+      if (ord($s2[0]) != 0xbf) return $s . $s2 . fgets($fp,4096);
+    }
+  }
+  if ($state < 0) return fgets($fp,4096);
+
+  $ret = "";
+  while ($s = fread($fp,2))
+  {
+    if ($state == 1) $s = ord($s[0]) | (ord($s[1])<<8);
+    else $s = ord($s[1]) | (ord($s[0])<<8);
+    if ($s < 128) 
+    {
+      $ret .= chr($s);
+      if ($s == ord("\n")) break;
+    }
+    else if ($s <= 0x7ff) $ret .= chr(0xc0 | (($s>>6)&0x1f)) . chr(0x80 | ($s&0x3f));
+    else $ret .= chr(0xe0 | (($s>>12)&0xf)) . chr(0x80 | (($s)>>6)&0x3f) . chr(0x80 | ($s&0x3f));
+  }
+
+  return $ret;
+}
+
 function swell_rc2cpp_dialog($fp) // returns array with ["data"] and optionally ["error"]
 {
   fseek($fp,0,SEEK_SET);
+  $utf_state=0;
   $errstr="";
   $retstr = "";
 
@@ -67,7 +102,7 @@ function swell_rc2cpp_dialog($fp) // returns array with ["data"] and optionally 
   for (;;) 
   {
     if ($next_line != "") { $x=$next_line; $next_line =""; }
-    else if (!($x=fgets($fp))) break;
+    else if (!($x=utf_fgets($fp,$utf_state))) break;
     $x = convertquotes($x);
 
     $y=trim($x);
@@ -139,7 +174,7 @@ function swell_rc2cpp_dialog($fp) // returns array with ["data"] and optionally 
               $rep=0;
               for (;;) 
               {
-                $next_line = fgets($fp,4096);
+                $next_line = utf_fgets($fp,$utf_state);
                 if (!($next_line )) { $next_line=""; break; }
                 if (substr($next_line,0,1)==" " || substr($next_line,0,1)=="\t")
                 {
@@ -193,10 +228,11 @@ function swell_rc2cpp_menu($fp) // returns array with ["data"] and optionally ["
   $errstr="";
 
   fseek($fp,0,SEEK_SET);
+  $utf_state=0;
 
   $menu_symbol="";
   $menu_depth=0;
-  while (($x=fgets($fp)))
+  while (($x=utf_fgets($fp,$utf_state)))
   {
     $x = convertquotes($x);
 
