964b91492452e633164e081857597ffd0e2e8c7f
Cockos Inc <nospam@cockos.com>
2017-10-25 10:14:49 -0400

    simplified win32_helpers -- from 86045051

--- WDL/win32_helpers.h
+++ WDL/win32_helpers.h
@@ -1,26 +1,26 @@
 #ifndef _WDL_WIN32_HELPERS_H_
 #define _WDL_WIN32_HELPERS_H_
 
-// helpers to make some tedious APIs a little helper
 
 static HMENU InsertSubMenu(HMENU hMenu, int pos, const char *name, int flags=0)
 {
-  if (!hMenu) return NULL;
-  MENUITEMINFO mii = { sizeof(mii), MIIM_STATE|MIIM_TYPE|MIIM_SUBMENU, MFT_STRING, flags, 0, CreatePopupMenu(), NULL, NULL, 0, (char*)name };
-  InsertMenuItem(hMenu,pos,TRUE,&mii);
-  return mii.hSubMenu;
+  HMENU sub = NULL;
+  if (hMenu) 
+  {
+    sub = CreatePopupMenu();
+    InsertMenu(hMenu,pos,flags|MF_BYPOSITION|(sub?MF_POPUP:0)|MF_STRING,(UINT_PTR)sub, name);
+  }
+  return sub;
 }
 
 static void InsertMenuString(HMENU hMenu, int pos, const char *name, int idx, int flags=0)
 {
-  MENUITEMINFO mii = { sizeof(mii), MIIM_STATE|MIIM_TYPE|MIIM_ID, MFT_STRING, flags, idx, NULL, NULL, NULL, 0, (char*)name };
-  if (hMenu) InsertMenuItem(hMenu,pos,TRUE,&mii);
+  if (hMenu) InsertMenu(hMenu,pos,flags|MF_BYPOSITION|MF_STRING, idx, name);
 }
 
 static void InsertMenuSeparator(HMENU hMenu, int pos)
 {
-  MENUITEMINFO mii = { sizeof(mii), MIIM_TYPE, MFT_SEPARATOR, };
-  if (hMenu) InsertMenuItem(hMenu,pos,TRUE,&mii);
+  if (hMenu) InsertMenu(hMenu,pos,MF_BYPOSITION|MF_SEPARATOR, 0, NULL);
 }
 
 #endif
