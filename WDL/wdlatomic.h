ef4e45735ac86da45de6bae1b0a378b4199ee894
Cockos Inc <nospam@cockos.com>
2018-01-16 20:07:20 -0500

    add volatile wdl_atomic_incr/decr versions -- from 1de1aa03

--- WDL/wdlatomic.h
+++ WDL/wdlatomic.h
@@ -5,11 +5,15 @@
 
 static int wdl_atomic_incr(int *v) { return (int) InterlockedIncrement((LONG *)v); }
 static int wdl_atomic_decr(int *v) { return (int) InterlockedDecrement((LONG *)v); }
+static int wdl_atomic_incr(volatile int *v) { return (int) InterlockedIncrement((LONG *)v); }
+static int wdl_atomic_decr(volatile int *v) { return (int) InterlockedDecrement((LONG *)v); }
 
 #elif (!defined(__APPLE__) || !defined(__ppc__)) && (defined(__clang__) || (defined(__GNUC__) && (__GNUC__ > 4 || (__GNUC__ == 4 && __GNUC_MINOR__ >= 2))))
 
 static int wdl_atomic_incr(int *v) { return __sync_add_and_fetch(v,1); }
 static int wdl_atomic_decr(int *v) { return __sync_add_and_fetch(v,~0); }
+static int wdl_atomic_incr(volatile int *v) { return __sync_add_and_fetch(v,1); }
+static int wdl_atomic_decr(volatile int *v) { return __sync_add_and_fetch(v,~0); }
 
 #elif defined(__APPLE__)
 // used by GCC < 4.2 on OSX
@@ -17,6 +21,8 @@ static int wdl_atomic_decr(int *v) { return __sync_add_and_fetch(v,~0); }
 
 static int wdl_atomic_incr(int *v) { return (int) OSAtomicIncrement32Barrier((int32_t*)v); }
 static int wdl_atomic_decr(int *v) { return (int) OSAtomicDecrement32Barrier((int32_t*)v); }
+static int wdl_atomic_incr(volatile int *v) { return (int) OSAtomicIncrement32Barrier((int32_t*)v); }
+static int wdl_atomic_decr(volatile int *v) { return (int) OSAtomicDecrement32Barrier((int32_t*)v); }
 #else
 
 // unsupported! 
