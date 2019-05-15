fe129ab7f5142b06f65c413d140ba988a827ff3c
Cockos Inc <nospam@cockos.com>
2019-05-15 09:43:25 -0400

    eel_lice: set window classname on non-win32 -- from 896e5cfc

--- WDL/eel2/eel_lice.h
+++ WDL/eel2/eel_lice.h
@@ -1896,10 +1896,8 @@ void eel_lice_register()
 
 #ifdef _WIN32
 static HINSTANCE eel_lice_hinstance;
-static const char *eel_lice_standalone_classname;
 #endif
-
-HWND eel_lice_standalone_owner;
+static const char *eel_lice_standalone_classname;
 
 #ifdef EEL_LICE_WANT_STANDALONE_UPDATE
 static EEL_F * NSEEL_CGEN_CALL _gfx_update(void *opaque, EEL_F *n)
@@ -2119,7 +2117,10 @@ HWND eel_lice_state::create_wnd(HWND par, int isChild)
   return CreateWindowEx(WS_EX_ACCEPTFILES,eel_lice_standalone_classname,"",
                         isChild ? (WS_CHILD|WS_TABSTOP) : (WS_POPUP|WS_CAPTION|WS_THICKFRAME|WS_SYSMENU),CW_USEDEFAULT,CW_USEDEFAULT,100,100,par,NULL,eel_lice_hinstance,this);
 #else
-  return SWELL_CreateDialog(NULL,isChild ? NULL : ((const char *)(INT_PTR)0x400001),par,(DLGPROC)eel_lice_wndproc,(LPARAM)this);
+  HWND h = SWELL_CreateDialog(NULL,isChild ? NULL : ((const char *)(INT_PTR)0x400001),par,(DLGPROC)eel_lice_wndproc,(LPARAM)this);
+  if (h)
+    SWELL_SetClassName(h,eel_lice_standalone_classname);
+  return h;
 #endif
 }
 
@@ -2648,12 +2649,12 @@ LRESULT WINAPI eel_lice_wndproc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lPar
 
 void eel_lice_register_standalone(HINSTANCE hInstance, const char *classname, HWND hwndPar, HICON icon)
 {
+  eel_lice_standalone_classname=classname && *classname ? classname : "EEL_LICE_gfx_standalone";
 #ifdef _WIN32
   static bool reg;
   if (!reg)
   {
     eel_lice_hinstance=hInstance;
-    eel_lice_standalone_classname=classname && *classname ? classname : "EEL_LICE_gfx_standalone";
     WNDCLASS wc={CS_DBLCLKS,eel_lice_wndproc,0,0,hInstance,icon,LoadCursor(NULL,IDC_ARROW), NULL, NULL,eel_lice_standalone_classname};
     RegisterClass(&wc);
     reg = true;
