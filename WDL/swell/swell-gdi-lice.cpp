59476ae51a9362677e4b934b3aa13aa52bc0bb12
Cockos Inc <nospam@cockos.com>
2019-06-11 14:50:42 -0400

    swell-lice: fix freetype tmInternalLeading calculation -- from 130de804

--- WDL/swell/swell-gdi-lice.cpp
+++ WDL/swell/swell-gdi-lice.cpp
@@ -1001,8 +1001,8 @@ BOOL GetTextMetrics(HDC ctx, TEXTMETRIC *tm)
     tm->tmDescent = face->size->metrics.descender/64;
     tm->tmHeight = face->size->metrics.height/64 + 1;
     tm->tmAveCharWidth = face->size->metrics.height / 112;
-    // hmm? some font freetype/win32 expert can weigh in here :/
-    tm->tmInternalLeading = (face->size->metrics.height - face->size->metrics.ascender)/64;
+
+    tm->tmInternalLeading = (face->size->metrics.ascender + face->size->metrics.descender - face->size->metrics.height)/64;
     if (tm->tmInternalLeading<0) tm->tmInternalLeading=0;
   }
 #endif
