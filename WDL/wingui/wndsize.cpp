26697a3356068afe15511a55aac2875ea3e3c3bc
Cockos Inc <nospam@cockos.com>
2019-08-05 12:53:56 -0400

    wdl_wndsizer: optimize region handling -- from 5ba60e48

--- WDL/wingui/wndsize.cpp
+++ WDL/wingui/wndsize.cpp
@@ -141,10 +141,10 @@ void WDL_WndSizer::init_item(int dlg_id, float left_scale, float top_scale, floa
 BOOL CALLBACK WDL_WndSizer::enum_RegionRemove(HWND hwnd,LPARAM lParam)
 {
   WDL_WndSizer *_this=(WDL_WndSizer *)lParam;
-//  if(GetParent(hwnd)!=_this->m_hwnd) return TRUE;
     
   if (IsWindowVisible(hwnd)) 
   {
+    if(GetParent(hwnd)!=_this->m_hwnd) return TRUE;
     RECT r;
     GetWindowRect(hwnd,&r);
     if (_this->m_hwnd)
@@ -152,9 +152,13 @@ BOOL CALLBACK WDL_WndSizer::enum_RegionRemove(HWND hwnd,LPARAM lParam)
       ScreenToClient(_this->m_hwnd,(LPPOINT)&r);
       ScreenToClient(_this->m_hwnd,((LPPOINT)&r)+1);
     }
-    HRGN rgn2=CreateRectRgn(r.left,r.top,r.right,r.bottom);
-    CombineRgn(_this->m_enum_rgn,_this->m_enum_rgn,rgn2,RGN_DIFF);
-    DeleteObject(rgn2);
+    if (RectInRegion(_this->m_enum_rgn,&r))
+    {
+      HRGN rgn2=CreateRectRgnIndirect(&r);
+      const int ret = CombineRgn(_this->m_enum_rgn,_this->m_enum_rgn,rgn2,RGN_DIFF);
+      DeleteObject(rgn2);
+      if (ret == NULLREGION || ret == ERROR) return FALSE;
+    }
   }
   
   return TRUE;
@@ -255,8 +259,7 @@ void WDL_WndSizer::onResize(HWND only, int notouch, int xtranslate, int ytransla
   GetClientRect(m_hwnd,&new_rect);
 #ifdef _WIN32
 
-  m_enum_rgn=CreateRectRgn(new_rect.left,new_rect.top,new_rect.right,new_rect.bottom);
- // EnumChildWindows(m_hwnd,enum_RegionRemove,(LPARAM)this);
+  m_enum_rgn=CreateRectRgnIndirect(&new_rect);
   
   HDWP hdwndpos=NULL;
   int has_dfwp=0;
