9c496bb3b37011c837cfc0559743cb89f15fb903
Cockos Inc <nospam@cockos.com>
2021-08-12 11:41:04 -0400

    coolscroll: handle more page/min/max corner cases gracefully -- from 144ec552

--- WDL/wingui/scrollbar/coolscroll.cpp
+++ WDL/wingui/scrollbar/coolscroll.cpp
@@ -3629,8 +3629,9 @@ int  WINAPI CoolSB_SetScrollInfo (HWND hwnd, int fnBar, LPSCROLLINFO lpsi, BOOL 
   //The nPos member must specify a value between nMin and nMax - wdl_max(nPage - 1, 0).
   if(lpsi->fMask & SIF_POS)
   {
-    mysi->nPos = wdl_max(lpsi->nPos, mysi->nMin);
-    mysi->nPos = wdl_min(mysi->nPos, mysi->nMax - wdl_max((int) mysi->nPage - 1, 0));
+    int maxv = mysi->nMax - wdl_max((int) mysi->nPage - 1, 0);
+    if (maxv < mysi->nMin) maxv = mysi->nMin;
+    mysi->nPos = wdl_clamp(lpsi->nPos, mysi->nMin, maxv);
   }
 
   sbar = GetScrollBarFromHwnd(hwnd, fnBar);
@@ -3677,7 +3678,7 @@ int  WINAPI CoolSB_SetScrollInfo (HWND hwnd, int fnBar, LPSCROLLINFO lpsi, BOOL 
 int WINAPI CoolSB_SetScrollPos(HWND hwnd, int nBar, int nPos, BOOL fRedraw)
 {
   SCROLLINFO *mysi;
-  int oldpos;
+  int oldpos,maxv;
 
   if(!(mysi = GetScrollInfoFromHwnd(hwnd, nBar)))
   {
@@ -3689,9 +3690,11 @@ int WINAPI CoolSB_SetScrollPos(HWND hwnd, int nBar, int nPos, BOOL fRedraw)
   //  return mysi->nPos;
 
   //validate and set the scollbar position
+  maxv = mysi->nMax - wdl_max((int) mysi->nPage - 1, 0);
+  if (maxv < mysi->nMin) maxv = mysi->nMin;
+
   oldpos = mysi->nPos;
-  mysi->nPos = wdl_max(nPos, mysi->nMin);
-  mysi->nPos = wdl_min(mysi->nPos, mysi->nMax - wdl_max((int)mysi->nPage - 1, 0));
+  mysi->nPos = wdl_clamp(nPos, mysi->nMin, maxv);
 
   if(fRedraw && !CoolSB_IsThumbTracking(hwnd))
     RedrawNonClient(hwnd, FALSE);
