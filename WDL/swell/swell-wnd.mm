11f1a0e9d761e744e764ff0f28d30da70b725adf
Cockos Inc <nospam@cockos.com>
2021-11-30 17:59:45 -0500

    swell-cocoa: can define SWELL_COCOA_WILL_HAVE_PREBIGSUR_SDK if compiling against a 11.0+ SDK but will change the SDK version post-build using vtool -set-version-min -- from 24f28a60

--- WDL/swell/swell-wnd.mm
+++ WDL/swell/swell-wnd.mm
@@ -3708,7 +3708,7 @@ void SWELL_UnregisterCustomControlCreator(SWELL_ControlCreatorProc proc)
 static void set_listview_bigsur_style(NSTableView *obj)
 {
   if (SWELL_GetOSXVersion() < 0x1100) return;
-#ifdef MAC_OS_VERSION_11_0
+#if defined(MAC_OS_VERSION_11_0) && (!defined(SWELL_COCOA_WILL_HAVE_PREBIGSUR_SDK) || defined(__aarch64__))
   // newer SDKs default to NSTableViewStyleAutomatic
   int style = (g_swell_osx_style & 2) ? -1 : 4 /* NSTableViewStylePlain */;
 #else
